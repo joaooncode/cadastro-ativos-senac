282385f7b56df9fe776ac4f0620965fd91a5a152
João Vitor R. da Silva <joaovitor.r.silva.3@gmail.com>
2024-11-26 22:03:31 -0300

    TODO: INSERT db

--- controllers/assets.php
+++ controllers/assets.php
@@ -2,6 +2,7 @@
 
 
 include_once('../models/connect_db.php');
+include_once('session.php');
 
 $description = $_POST['description'];
 $quantity = $_POST['quantity'];
@@ -9,3 +10,28 @@ $obs = $_POST['obs'];
 $status = $_POST['status'];
 $brand = $_POST['brand'];
 $type = $_POST['type'];
+$user = $_SESSION['user_id'];
+
+
+$query = "
+    INSERT INTO ativo(
+        idMarca,
+        idTipo,
+        descricaoAtivo,
+        statusAtivo,
+        quantidadeAtivo,
+        obsAtivo,
+        dataHoraCadastroAtivo 
+    ) values(
+        '" . $brand . "',
+        '" . $type . "',
+        '" . $description . "',
+        '" . $status . "',
+        '" . $quantity . "',
+        '" . $obs . "',
+        NOW()
+    )
+";
+
+
+$result = mysqli_query($conn, $query) or die(false);

297e95db0e8634a86ecf06add3f4979fb81b82ac
João Vitor R. da Silva <joaovitor.r.silva.3@gmail.com>
2024-11-29 21:39:33 -0300

    Fix: List assets layout

--- view/register_assets.php
+++ view/register_assets.php
@@ -5,7 +5,6 @@ include_once('dropdown.php');
 include_once('../models/connect_db.php');
 include_once('../controllers/functions.php');
 
-
 $brands = fetchData($conn, 'marca');
 $types = fetchData($conn, 'tipo');
 
@@ -13,13 +12,12 @@ include_once('modal.php');
 
 $assets = fetchData($conn, 'ativo');
 
-
 $sql = "
     SELECT 
     idAtivo,
     descricaoAtivo,
-    (SELECT descricaoMarca FROM marca m WHERE m.idMarca = a.idMarca) as marca
-    , statusAtivo,
+    (SELECT descricaoMarca FROM marca m WHERE m.idMarca = a.idMarca) as marca,
+    statusAtivo,
     dataHoraCadastroAtivo, 
     quantidadeAtivo,
     obsAtivo,
@@ -34,90 +32,44 @@ $data = $result->fetch_all(MYSQLI_ASSOC);
 
 ?>
 
-<body class="min-vw-100 min-vh-100 overflow-hidden">
-    <div class="container min-vh-100 min-vw-100 d-flex align-content-center justify-content-center flex-column">
+<body class="min-vw-100 min-vh-100">
+    <div class="container-fluid min-vh-100 d-flex align-items-center justify-content-center flex-column">
         <main class="vw-100 vh-100 d-flex align-items-center justify-content-center flex-column">
             <!--Tabela ativos cadastrados-->
-            <div class="container mb-5 w-100">
+            <div class="container-fluid mb-5 w-100">
                 <h1 class="text-center text-primary">Lista de Ativos</h1>
-                <table class="table table-bordered  border-primary mt-5">
-                    <table class="table table-bordered  border-primary mt-5">
+
+                <!-- Adicionando responsividade à tabela -->
+                <div class="table-responsive">
+                    <table class="table table-bordered border-primary mt-5">
                         <thead>
-                            <th scope="col">
-                                Código
-                            <th scope="col">
-                                Descrição
-                            </th>
-                            <th scope="col">
-                                Quantidade
-                            </th>
-                            <th scope="col">
-                                Marca
-                            </th>
-                            <th scope="col">
-                                Status
-                            </th>
-                            <th scope="col">
-                                Observação
-                            </th>
-                            <th scope="col">
-                                Tipo
-                            </th>
-                            <th scope="col">
-                                Data de Cadastro
-                            </th>
-                            <th scope="col">
-                                Ações
-                            </th>
+                            <tr>
+                                <th scope="col">Código</th>
+                                <th scope="col">Descrição</th>
+                                <th scope="col">Quantidade</th>
+                                <th scope="col">Marca</th>
+                                <th scope="col">Status</th>
+                                <th scope="col">Observação</th>
+                                <th scope="col">Tipo</th>
+                                <th scope="col">Data de Cadastro</th>
+                                <th scope="col">Ações</th>
+                            </tr>
                         </thead>
                         <tbody>
                             <?php
                             foreach ($data as $assets => $value) {
                                 ?>
                                 <tr>
-
+                                    <td><?php echo $value['idAtivo'] ?></td>
+                                    <td><?php echo $value['descricaoAtivo'] ?></td>
+                                    <td><?php echo $value['quantidadeAtivo'] ?></td>
+                                    <td><?php echo $value['marca'] ?></td>
+                                    <td><?php echo $value['statusAtivo'] ?></td>
+                                    <td><?php echo $value['obsAtivo'] ?></td>
+                                    <td><?php echo $value['tipo'] ?></td>
+                                    <td><?php echo $value['dataHoraCadastroAtivo'] ?></td>
                                     <td>
-                                        <p>
-                                            <?php echo $value['idAtivo'] ?>
-                                        </p>
-                                    </td>
-                                    <td>
-                                        <p>
-                                            <?php echo $value['descricaoAtivo'] ?>
-                                        </p>
-                                    </td>
-                                    <td>
-                                        <p>
-                                            <?php echo $value['quantidadeAtivo'] ?>
-                                        </p>
-                                    </td>
-                                    <td>
-                                        <p>
-                                            <?php echo $value['marca'] ?>
-                                        </p>
-                                    </td>
-                                    <td>
-                                        <p>
-                                            <?php echo $value['statusAtivo'] ?>
-                                        </p>
-                                    </td>
-                                    <td>
-                                        <p>
-                                            <?php echo $value['obsAtivo'] ?>
-                                        </p>
-                                    </td>
-                                    <td>
-                                        <p>
-                                            <?php echo $value['tipo'] ?>
-                                        </p>
-                                    </td>
-                                    <td>
-                                        <p>
-                                            <?php echo $value['dataHoraCadastroAtivo'] ?>
-                                        </p>
-                                    </td>
-                                    <td>
-                                        <button class="btn  btn-primary">
+                                        <button class="btn btn-primary">
                                             <i class="bi bi-pencil-square"></i>
                                         </button>
                                     </td>
@@ -127,9 +79,13 @@ $data = $result->fetch_all(MYSQLI_ASSOC);
                             ?>
                         </tbody>
                     </table>
+                </div> <!-- Fim da table-responsive -->
             </div>
-            <button style="width: 15%;" type="button" class="btn btn-outline-primary mt-3 mb-5  p-3"
+
+            <!-- Botão responsivo com largura ajustada para dispositivos móveis -->
+            <button style="width: 100%; max-width: 200px;" type="button" class="btn btn-outline-primary mt-3 mb-5 p-3"
                 data-bs-toggle="modal" data-bs-target="#exampleModal">Cadastrar Ativo</button>
+
         </main>
     </div>
 </body>
